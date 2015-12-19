e128e1a9f55692f674260be82e3368b5e53227ce
Dmitry Duev <dmitry.duev@gmail.com>
2015-12-19 04:43:29 -0700

    added a png-generating daemon.

--- server-daemon.py
+++ server-daemon.py
@@ -18,24 +18,6 @@ if __name__ == '__main__':
     status = subprocess.Popen('./status-daemon.sh', shell=True)
     
     # finally, run the png generating engine
-#    png = subprocess.Popen('./png-daemon.sh', shell=True)
+    png = subprocess.Popen('./png-daemon.sh', shell=True)
 #    png = subprocess.Popen('watch -n 0.5 ./lib/png2 ', shell=True)
     
-#    sig1, sig2 = 1, 1
-#    while sig1 != 0 and sig2 != 0:
-#        time.sleep(2)
-#        server = subprocess.Popen('python server.py', shell=True)
-#        time.sleep(2)
-#        status = subprocess.Popen('python status.py', shell=True)
-#        sig1 = server.wait()
-#        sig2 = status.wait()
-#        print 'lala'*100, sig1
-    
-#    server = subprocess.Popen('python server.py', shell=True)
-#    
-#    sig = server.wait()
-#    if sig != 0:
-#        time.sleep(2)
-#        
-#    print 'lala'*100, sig
-#    server.communicate()
